db1a06941965194eef5899538641989318569040
JS1331 <jansarda2003@gmail.com>
2024-12-11 20:10:32 +0100

    Mandoguilla

---
+++ Seriousgame2024/app/src/main/java/com/example/seriousgame2024/PokedexActivity.kt
@@ -0,0 +1,11 @@
+package com.example.seriousgame2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokedexActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pokedex)
+    }
+}
