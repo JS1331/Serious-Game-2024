2754cda0c62eff9d9b17fcee9ef159c43c6acd65
marcmc <marc.marza@epiaedu.cat>
2025-01-17 18:40:27 +0100

    poke all

---
+++ Seriousgame2024/app/src/main/java/com/example/seriousgame2024/StatBattleGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.seriousgame2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatBattleGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stat_battle_game)
+    }
+}
