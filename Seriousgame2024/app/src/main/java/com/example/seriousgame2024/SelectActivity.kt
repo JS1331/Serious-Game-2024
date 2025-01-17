ca3abdd13109f84637d0603caa0ff50b43b319b4
JS1331 <jansarda2003@gmail.com>
2025-01-17 17:26:42 +0100

    trivia

--- Seriousgame2024/app/src/main/java/com/example/seriousgame2024/SelectActivity.kt
+++ Seriousgame2024/app/src/main/java/com/example/seriousgame2024/SelectActivity.kt
@@ -1,26 +1,32 @@
 package com.example.seriousgame2024
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.snackbar.Snackbar
 
 class SelectActivity : AppCompatActivity() {
-    lateinit var btnSafariBall: Button
-    lateinit var btnPokeBall: Button
-    lateinit var btnSuperBall: Button
-    lateinit var btnUltraBall: Button
-    lateinit var btnMasterBall: Button
+    lateinit var btnSafariBall: ImageButton
+    lateinit var btnPokeBall: ImageButton
+    lateinit var btnSuperBall: ImageButton
+    lateinit var btnUltraBall: ImageButton
+    lateinit var btnMasterBall: ImageButton
+    var maxScore: Int = 0
+    private val REQUEST_CODE_JOC_ACTIVITY = 1
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
-        btnSafariBall = findViewById<Button>(R.id.btnSafariBall)
-        btnPokeBall = findViewById<Button>(R.id.btnPokeBall)
-        btnSuperBall = findViewById<Button>(R.id.btnSuperBall)
-        btnUltraBall = findViewById<Button>(R.id.btnUltraBall)
-        btnMasterBall = findViewById<Button>(R.id.btnMasterBall)
+        btnSafariBall = findViewById<ImageButton>(R.id.btnSafariBall)
+        btnPokeBall = findViewById<ImageButton>(R.id.btnPokeBall)
+        btnSuperBall = findViewById<ImageButton>(R.id.btnSuperBall)
+        btnUltraBall = findViewById<ImageButton>(R.id.btnUltraBall)
+        btnMasterBall = findViewById<ImageButton>(R.id.btnMasterBall)
 
         btnSafariBall.setOnClickListener(){
             goToTrivia()
@@ -43,8 +49,23 @@ class SelectActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_JOC_ACTIVITY && resultCode == Activity.RESULT_OK) {
+            data?.let {
+                maxScore = it.getIntExtra("max_score", 0)
+                Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "New max score received: $maxScore",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
     private fun goToTrivia() {
         val intent = Intent(this,TriviaGameActivity::class.java)
+        intent.putExtra("max_score", maxScore)
         startActivity(intent)
     }
 
--- Seriousgame2024/app/src/main/java/com/example/seriousgame2024/classes/Pokemon.kt
+++
@@ -1,8 +0,0 @@
-package com.example.seriousgame2024.classes
-
-data class Pokemons(
-    val pokemons: List<Pokemon>
-)
-data class Pokemon(
-    val name: String,
-)
